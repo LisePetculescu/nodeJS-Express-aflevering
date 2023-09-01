6bd33498a45f7253f824b3306f612842e740f88b
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-09-01 22:07:52 +0200

    can now get the artist list from the json file through requests

--- Backend/script.js
+++ Backend/script.js
@@ -1,9 +1,26 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs/promises";
 
-const app = express(); 
+const app = express();
+const port = 3000;
 
-app.listen(3000, () => {
-    console.log("noget sjovt skete... server kører på port 3000");
-}); 
+app.listen(port, () => {
+  console.log(`noget sjovt skete... server kører på http://localhost:${port}`);
+});
 
+app.get("/", (req, res) => {
+  res.send("vi får respons! 🎉👌");
+});
+
+app.get("/artists", async (req, res) => {
+  //   res.send("vi får artister! 🎉");
+  const artistList = await fs.readFile("artistData.json");
+  const artists = JSON.parse(artistList);
+  res.json(artists);
+  //   console.log(artists);
+});
+
+http: app.get("/artist/:id", (req, res) => {
+  res.send("vi får respons! med id 👌");
+});
